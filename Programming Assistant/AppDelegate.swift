50ed18cbc97a8092a68200d70fb759a65e2c020e
spectatorNan <spectatornan@gmail.com>
2018-09-28 23:18:01 +0800

    Initial Commit

---
+++ Programming Assistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Programming Assistant
+//
+//  Created by spectator Mr.Z on 2018/9/28.
+//  Copyright © 2018 Mr.Z. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Programming Assistant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Programming Assistant
+//
+//  Created by spectator Mr.Z on 2018/9/28.
+//  Copyright © 2018 Mr.Z. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
